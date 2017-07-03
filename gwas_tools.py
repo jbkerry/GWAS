210c6244db9e7ff9e198e280741a20d87cb1b34e
Jon Kerry <jon.kerry@imm.ox.ac.uk>
2017-07-03 16:25:11 +0100

    modified gwas_tools to accommodate imputed SNPs

--- gwas_tools.py
+++ gwas_tools.py
@@ -7,38 +7,41 @@ import subprocess
 import pandas as pd
 import numpy as np
 
-jh = pd.read_table('JH_vDH-SNPinfo_c1.txt', header=1)
-cd = pd.read_table('CD_vDH-SNPinfo_c1.txt', header=1)
-st = pd.read_table('ST_vDH-SNPinfo_c1.txt', header=1)
-df_dict = {'JH': jh, 'CD': cd, 'ST': st}
-
-def get_snp_coor(snp_file, bed_dir):
+def get_snp_coor(ext, snp_file, bed_dir):
+    df_dict = {'JH': '', 'CD': '', 'ST': ''}
+    for key in df_dict:
+        df = pd.read_table(key+ext, header=1)
+        df_dict[key] = df
     bed_files = [x for x in os.listdir(bed_dir) if x.endswith('.bed')]
     p = re.compile('_(?P<initials>[CJS][DHT])_*_(?P<day>d\d{1,2})_')
     for x in bed_files:
         m = p.search(x)
         bed_file = os.path.join(bed_dir, x)
+        snp_file = '{}_impSNPs.bed'.format(m.group('initials')) # remove this line if running on sentinel SNPs
         bedtools = subprocess.run(
             'module load bedtools\nbedtools intersect -a {} -b {}'.format(
                 snp_file,
                 bed_file),
             shell=True, stdout=subprocess.PIPE)
         out = bedtools.stdout.decode('utf-8')
-        data = np.array([x.split('\t') for x in out.strip().split('\n')])
+        #data = np.array([x.split('\t') for x in out.strip().split('\n')])
+        data = np.array([x.split('\t') for x in out.split('\n')][:-1])
+        #print(data)
         if len(data)>0:
-            atac = np.where([any(x==data[:,3]) for x in jh['SNP name']],
+            atac = np.where([any(x==data[:,3]) for x in df_dict[m.group('initials')]['SNP name']],
                             'Yes', 'No')
+            #print('high')
         else:
             atac = ['No']*df_dict[m.group('initials')].shape[0]
+            #print('low')
         new_col = 'ATAC_{}'.format(m.group('day'))
         df_dict[m.group('initials')][new_col] = atac
         
     return df_dict
     
-def write_df(df):
+def write_df(df, out_ext):
     for x in df:
-        file_name = '{}_ATAC_intersect.txt'.format(x)
-        df[x].to_csv(file_name, index=False, sep='\t')
+        df[x].to_csv(x+out_ext, index=False, sep='\t')
     
     return
 
