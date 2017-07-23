680f12cacdf4ded9e4918a7afc3c51b02bcf1327
Jon Kerry <jon.kerry@imm.ox.ac.uk>
2017-07-23 21:04:07 +0100

    top 3 functions now operating in a class

--- linkage_snps.py
+++ linkage_snps.py
@@ -13,37 +13,8 @@ import argparse
 import pysam
 from scipy import stats
 
-def group_imputations():
-    """Generates dictionary with every proximal SNP as a key referencing
-    a dictionary with every linked imputed SNP as key with that SNP's
-    location and base change as a value
-    
-    Returns
-    -------
-    imp_dict : dict
-        Contains imputed SNPs linked to proximal SNPs
-    
-    """
-    
-    imp_dict = {}
-    with open('/t1-data1/WTSA_Dev/jkerry/BloodATAC/Ron_imputed_SNPs.txt') as f:
-        for x in f:
-            (chr_name, start, stop, imp_snp, ref_al, alt_al,
-             pr_snp, desc, snp_type) = x.strip().split('\t')
-            if pr_snp not in imp_dict:
-                imp_dict[pr_snp] = {}
-            loc = ':'.join((chr_name, stop))
-            ref_change = '/'.join((ref_al, alt_al))
-            if (len(ref_al)==1) & (len(alt_al)==1):  # only include imputed SNP if it really is a SNP and not an indel
-                #imp_dict[pr_snp].update({imp_snp: '_'.join((loc, ref_change))})
-                imp_dict[pr_snp][imp_snp] = '_'.join((loc, ref_change))
-    
-    return imp_dict
-    
-
-def match_vdh_to_ngs(initials, length):
-    """Determines if any of the van De Harst 75 SNPs are in the
-    specified NGS sequencing file
+class DetectGwasSnps(object):
+    """Explanation here
     
     Parameters
     ----------
@@ -51,174 +22,217 @@ def match_vdh_to_ngs(initials, length):
         Initials of the individual who has been sequenced
     length : int
         Read length of the sequences
+    cutoff : int
+        The sequencing depth minimum cut-off to use
+    
+    Attributes
+    ----------
+    _bam : str
+        The path to the bam file to use to search for the SNPs
         
     """
     
-    cnames=('SNP', 'GWAS', 'A', 'C', 'T', 'G')
-    df = pd.DataFrame(columns=cnames)
+    def __init__(self, initials, length, cutoff):
+        self.initials = initials
+        self.length = length
+        self.cutoff = cutoff
+        self._bam = '/t1-data1/WTSA_Dev/jkerry/BloodATAC/ATAC_K4_{}.sorted.bam'.format(
+            self.initials
+        )
+
+
+    def group_imputations(self):
+        """Generates dictionary with every proximal SNP as a key referencing
+        a dictionary with every linked imputed SNP as key with that SNP's
+        location and base change as a value
+        
+        """
+        
+        self._imp_dict = {}
+        with open('/t1-data1/WTSA_Dev/jkerry/BloodATAC/'
+                  'Ron_imputed_SNPs.txt') as f:
+            for x in f:
+                (chr_name, start, stop, imp_snp, ref_al, alt_al,
+                 pr_snp, desc, snp_type) = x.strip().split('\t')
+                if pr_snp not in self._imp_dict:
+                    self._imp_dict[pr_snp] = {}
+                loc = ':'.join((chr_name, stop))
+                ref_change = '/'.join((ref_al, alt_al))
+                if (len(ref_al)==1) & (len(alt_al)==1):  # only include imputed SNP if it really is a SNP and not an indel
+                    self._imp_dict[pr_snp][imp_snp] = '_'.join((loc,
+                                                                ref_change))
+        
+        return
     
-    bam = '/t1-data1/WTSA_Dev/jkerry/BloodATAC/ATAC_K4_{}.sorted.bam'.format(
-        initials
-    )
-    
-    snp_dict = {}
-    info_dict = {}
-    
-    with open('/t1-data1/WTSA_Dev/jkerry/BloodATAC/'
-              'vanDeHarst2012_SNPs_hg19.txt') as f:
-        for x in f:
-            chr_name, snp, loc, base, genes = x.strip().split('\t')
-            snp_dict[snp] = {}
-            coor = ':'.join((chr_name, loc))
-            info_dict[snp] = coor
-            ea, oa = base.split('/')
-            chr_num = int(chr_name[3:])
-            loc = int(loc)
-            start = loc - length
-            stop = loc
+    def match_vdh_to_ngs(self):
+        """Determines if any of the van De Harst 75 SNPs are in the
+        specified NGS sequencing file
             
-            sf = pysam.AlignmentFile(bam, 'rb')
-            for r in sf.fetch(chr_name, start, stop):
-                pos = loc-(r.reference_start+1)
-                seq = r.query_sequence
-                if r.cigartuples[0][0]==0:
-                    m_len = r.cigartuples[0][1]-1
-                    if (pos>=0) & (pos<=m_len):
-                        seq_base = seq[pos]
-                        snp_dict[snp][seq_base] = snp_dict[snp].get(
-                            seq_base, 0) + 1
-                        
-            if snp_dict[snp]:
-                GWASCount = 0
-                nGWASCount = 0
-                base_dict = {'A': 0, 'C': 0, 'T': 0, 'G': 0}
-                for snp_base in snp_dict[snp]:
-                    if snp_base in base_dict:
-                        base_dict[snp_base]+=snp_dict[snp][snp_base]
-                df = df.append({'SNP': snp,
-                                'GWAS': ea,
-                                'A': base_dict['A'],
-                                'C': base_dict['C'],
-                                'T': base_dict['T'],
-                                'G': base_dict['G']}, ignore_index=True)
-    
-    return df, info_dict
-
-
-## at this point the script has read to each read and matched it to a SNP location where possible. For a given SNP it has calculated how many times the nucleotide matches that in the vanDeHarst paper or one of the other 3 nucleotides.
-## the dataframe also contains the 'replicate' i.e. the different day of the experiment (not really a replicate so this should be fixed later when taking averages) and the initials of the person
-
-def get_genotype(initials, cutoff, length, info_dict, imp_dict, data):
-
-    bam = '/t1-data1/WTSA_Dev/jkerry/BloodATAC/ATAC_K4_{}.sorted.bam'.format(
-        initials
-    )
-    imp_snp_out = open('{}_Hap_c{}_refac.bed'.format(initials, cutoff), 'w')
-    pr_snp_out = open('{}_vDH_SNPs_c{}_refac.bed'.format(initials, cutoff), 'w')
-    ColumnList = ['A','C','T','G']
-    ATots = []
-    CTots = []
-    TTots = []
-    GTots = []
-    user_dict = {}
-    code_dict = {}
-    for i, row in data.iterrows():
+        """
         
-        binary = np.where(row[['A', 'C', 'T', 'G']]>cutoff, 1, 0)  # Check sequencing depth of each recorded base
-        total = sum(binary)
-        percentage = [x/total*100 for x in binary]
-        ATots.append(percentage[0])
-        CTots.append(percentage[1])
-        TTots.append(percentage[2])
-        GTots.append(percentage[3])
-
-        status = 'N'
-        imp_snp_num = len(imp_dict[row['SNP']])
-        imp_yes = 0
-        imp_no = 0
-
-        pr_snp_chr, pr_snp_loc = info_dict[row['SNP']].split(':')
-        pr_snp_out.write("{}({})\n".format('\t'.join((pr_snp_chr,
-                                                      str(int(pr_snp_loc)-1),
-                                                      str(pr_snp_loc),
-                                                      row['SNP'])),
-                                           row['GWAS']))
+        cnames=('SNP', 'GWAS', 'A', 'C', 'T', 'G')
+        self.df = pd.DataFrame(columns=cnames)
         
-        # Check imputed SNPs here
+        snp_dict = {}
+        self._info_dict = {}
         
-        pr_snp_change = imp_dict[row['SNP']][row['SNP']].split('_')[1]
-        pr_ref,pr_alt = pr_snp_change.split('/')
-        
-        code = 0
-        if row[pr_ref]>cutoff: code += 1
-        if row[pr_alt]>cutoff: code += 10
-        code_dict[row['SNP']] = code
-        
-        if row[row['GWAS']]>cutoff: # Will only check imputed SNPs if vDH EA is present. Alter this IF statement to include other groups like the imputed SNP change, if different   
-            status = 'Y'
-            
-            for imp_snp in imp_dict[row['SNP']]:
-                imp_loc, imp_change = imp_dict[row['SNP']][imp_snp].split('_')
-                imp_chr, imp_coor = imp_loc.split(':')
-                imp_ref, imp_alt = imp_change.split('/')
-                alt_lengths = [len(x) for x in imp_alt.split(',')]
-                
-                loc = int(imp_coor)
-                start = loc - length
+        with open('/t1-data1/WTSA_Dev/jkerry/BloodATAC/'
+                  'vanDeHarst2012_SNPs_hg19.txt') as f:
+            for x in f:
+                chr_name, snp, loc, base, genes = x.strip().split('\t')
+                snp_dict[snp] = {}
+                coor = ':'.join((chr_name, loc))
+                self._info_dict[snp] = coor
+                ea, oa = base.split('/')
+                chr_num = int(chr_name[3:])
+                loc = int(loc)
+                start = loc - self.length
                 stop = loc
-                base_dict = {}
                 
-                sf = pysam.AlignmentFile(bam, 'rb')
-                for r in sf.fetch(imp_chr, start, stop):
+                sf = pysam.AlignmentFile(self._bam, 'rb')
+                for r in sf.fetch(chr_name, start, stop):
                     pos = loc-(r.reference_start+1)
                     seq = r.query_sequence
                     if r.cigartuples[0][0]==0:
                         m_len = r.cigartuples[0][1]-1
                         if (pos>=0) & (pos<=m_len):
-                            for x in alt_lengths:
-                                end_pos = pos + x
-                                seq_base = seq[pos:end_pos]
-                                base_dict[seq_base] = base_dict.get(
-                                    seq_base, 0) + 1
-                all_alts = imp_alt.split(',')
-                running_yes = 0
-                running_no = 0
-                for alt_al in all_alts:
-                    if alt_al not in base_dict:
-                        running_no += 1
-                    else:
-                        if base_dict[alt_al] > cutoff: running_yes += 1
-                        else: running_no += 1
+                            seq_base = seq[pos]
+                            snp_dict[snp][seq_base] = snp_dict[snp].get(
+                                seq_base, 0) + 1
                             
-                if running_yes>=1:
-                    value = 1000
-                    imp_yes += 1
-                else:
-                    value = 250
-                    imp_no += 1
+                if snp_dict[snp]:
+                    GWASCount = 0
+                    nGWASCount = 0
+                    base_dict = {'A': 0, 'C': 0, 'T': 0, 'G': 0}
+                    for snp_base in snp_dict[snp]:
+                        if snp_base in base_dict:
+                            base_dict[snp_base]+=snp_dict[snp][snp_base]
+                    self.df = self.df.append({'SNP': snp,
+                                               'GWAS': ea,
+                                               'A': base_dict['A'],
+                                               'C': base_dict['C'],
+                                               'T': base_dict['T'],
+                                               'G': base_dict['G']},
+                                             ignore_index=True)
+        
+        return
+    
+    
+    ## at this point the script has read to each read and matched it to a SNP location where possible. For a given SNP it has calculated how many times the nucleotide matches that in the vanDeHarst paper or one of the other 3 nucleotides.
+    ## the dataframe also contains the 'replicate' i.e. the different day of the experiment (not really a replicate so this should be fixed later when taking averages) and the initials of the person
+    
+    def get_genotype(self):
+    
+        imp_snp_out = open('{}_Hap_c{}_refac.bed'.format(self.initials, self.cutoff), 'w')
+        pr_snp_out = open('{}_vDH_SNPs_c{}_refac.bed'.format(self.initials, self.cutoff), 'w')
+
+        a_tot = []
+        c_tot = []
+        t_tot = []
+        g_tot = []
+        self._user_dict = {}
+        self._code_dict = {}
+        for i, row in self.df.iterrows():
+            
+            curr_snp = row['SNP']
+            curr_gwas = row['GWAS']
+            binary = np.where(row[['A', 'C', 'T', 'G']]>self.cutoff, 1, 0)  # Check sequencing depth of each recorded base
+            total = binary.sum()
+            if total != 0: percentage = binary/total * 100
+            else: percentage = (0, 0, 0, 0)
+            
+            a_tot.append(percentage[0])
+            c_tot.append(percentage[1])
+            t_tot.append(percentage[2])
+            g_tot.append(percentage[3])
+    
+            status = 'N'
+            imp_snp_num = len(self._imp_dict[curr_snp])
+            imp_yes = 0
+            imp_no = 0
+    
+            pr_snp_chr, pr_snp_loc = self._info_dict[curr_snp].split(':')
+            pr_snp_out.write("{}({})\n".format('\t'.join((pr_snp_chr,
+                                                          str(int(pr_snp_loc)-1),
+                                                          str(pr_snp_loc),
+                                                          curr_snp)),
+                                               curr_gwas))
+            
+            # Check imputed SNPs here
+            
+            pr_snp_change = self._imp_dict[curr_snp][curr_snp].split('_')[1]
+            pr_ref,pr_alt = pr_snp_change.split('/')
+            
+            code = 0
+            if row[pr_ref] > self.cutoff: code += 1
+            if row[pr_alt] > self.cutoff: code += 10
+            self._code_dict[curr_snp] = code
+            
+            if row[curr_gwas] > self.cutoff: # Will only check imputed SNPs if vDH EA is present. Alter this IF statement to include other groups like the imputed SNP change, if different   
+                status = 'Y'
+                
+                for imp_snp in self._imp_dict[curr_snp]:
+                    imp_loc, imp_change = self._imp_dict[curr_snp][imp_snp].split('_')
+                    imp_chr, imp_coor = imp_loc.split(':')
+                    imp_ref, imp_alt = imp_change.split('/')
+                    alt_lengths = [len(x) for x in imp_alt.split(',')]
                     
-                imp_pr_snp = '{}({})'.format(imp_snp, row['SNP'])
-                write_tup = (imp_chr, loc-1, loc, imp_pr_snp, value)
-                imp_snp_out.write('{}\n'.format('\t'.join(map(str, write_tup))))
-            
-            collate = '_'.join(map(str, (imp_snp_num, imp_yes, imp_no)))
-            ld_snp_num = {}
-            ld_snp_num[row['SNP']] = collate
-            
-        geno = 'Hom'
-        if total > 1:
-            geno = 'Het'
-        elif total == 0:
-            geno = "Ambiguous"
-            
-        string = ','.join((geno, status))
-        if geno == 'Ambiguous': string = geno
-        user_dict[row['SNP']] = string
-    
-    imp_snp_out.close()
-    pr_snp_out.close()
-    
-    return user_dict
+                    loc = int(imp_coor)
+                    start = loc - self.length
+                    stop = loc
+                    base_dict = {}
+                    
+                    sf = pysam.AlignmentFile(self._bam, 'rb')
+                    for r in sf.fetch(imp_chr, start, stop):
+                        pos = loc-(r.reference_start+1)
+                        seq = r.query_sequence
+                        if r.cigartuples[0][0]==0:
+                            m_len = r.cigartuples[0][1]-1
+                            if (pos>=0) & (pos<=m_len):
+                                for x in alt_lengths:
+                                    end_pos = pos + x
+                                    seq_base = seq[pos:end_pos]
+                                    base_dict[seq_base] = base_dict.get(
+                                        seq_base, 0) + 1
+                    all_alts = imp_alt.split(',')
+                    running_yes = 0
+                    running_no = 0
+                    for alt_al in all_alts:
+                        if alt_al not in base_dict:
+                            running_no += 1
+                        else:
+                            if base_dict[alt_al] > self.cutoff: running_yes += 1
+                            else: running_no += 1
+                                
+                    if running_yes >= 1:
+                        value = 1000
+                        imp_yes += 1
+                    else:
+                        value = 250
+                        imp_no += 1
+                        
+                    imp_pr_snp = '{}({})'.format(imp_snp, curr_snp)
+                    write_tup = (imp_chr, loc-1, loc, imp_pr_snp, value)
+                    imp_snp_out.write('{}\n'.format('\t'.join(map(str, write_tup))))
+                
+                collate = '_'.join(map(str, (imp_snp_num, imp_yes, imp_no)))
+                ld_snp_num = {}
+                ld_snp_num[curr_snp] = collate
+                
+            geno = 'Hom'
+            if total > 1:
+                geno = 'Het'
+            elif total == 0:
+                geno = "Ambiguous"
+                
+            string = ','.join((geno, status))
+            if geno == 'Ambiguous': string = geno
+            self._user_dict[curr_snp] = string
+        
+        imp_snp_out.close()
+        pr_snp_out.close()
+        
+        return
 
 ### To here
     
